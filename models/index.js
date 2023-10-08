7e276761616e00f7d45fb7e794d266fa4f1f5d21
VinaySavla <savlavinay77@gmail.com>
2023-10-08 19:38:09 +0530

    mysql2 solved

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+import mysql2 from 'mysql2';
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
@@ -13,11 +14,6 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-import mysql2 from 'mysql2';
-if (options.dialect === 'mysql') {
-  options.dialectModule = mysql2;
 }
 new Sequelize(options)
 fs
